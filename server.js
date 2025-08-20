6df15e01e8fec332fbc0ef3479db3f50c0e2482c
ian <cemenrttu@gmail.com>
2025-08-20 15:48:01 +0300

    Sever.js_changers

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(express.json());
 const hostname = '127.0.0.1';
 const port = process.env.PORT ;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Welcome to the GOMYCODE.');
 });
 
 
 
 app.listen(port, () => {
- console.log ('Server running at https://${hostname}:${port}/'); 
+ console.log (`Server running at https://${hostname}:${port}`); 
 })
 
 
